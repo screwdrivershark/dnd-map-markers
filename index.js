4383423fea5307baf8f4cdf3b042f8273db757c2
curious-broccoli <77789413+curious-broccoli@users.noreply.github.com>
2024-09-21 19:32:19 +0200

    enable multiple markers and add some

--- index.js
+++ index.js
@@ -23,25 +23,28 @@ fetch("markers.json")
             const verticalOffset = 20;
 
             const description = marker.text?.trim() || "";
-            const html = `
-            <button class="marker" style="top: ${marker.top - verticalOffset}px; left: ${marker.left - horizontalOffset}px;">
-                <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16"
-                    stroke="white" stroke-width="1">
-                    <!-- <animate attributeName="stroke-width" values="1;0;1" dur="4s" repeatCount="indefinite"/> -->
-                    <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6"/>                    
-                </svg>
-                <div class="popup hidden">
-                    <h2>${marker.title}</h2>
-                    <span>${description}</span>
-                </div>
-            </button>`;
-            mapContainer.innerHTML += html;
+            const button = document.createElement("button");
+            button.classList.add("marker");
+            button.style.left = `${marker.left - horizontalOffset}px`;
+            button.style.top = `${marker.top - verticalOffset}px`;
 
-            const button = mapContainer.querySelector(".marker");
+            const html = `
+            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16"
+                stroke="white" stroke-width="1">
+                <!-- <animate attributeName="stroke-width" values="1;0;1" dur="4s" repeatCount="indefinite"/> -->
+                <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6"/>                    
+            </svg>
+            <div class="popup hidden">
+                <h2>${marker.title}</h2>
+                <span>${description}</span>
+            </div>`;
+            button.innerHTML = html;
+
             button.addEventListener("click", (e) => {
                 const popup = e.currentTarget.querySelector(".popup");
                 popup.classList.toggle("hidden");
             });
+            mapContainer.appendChild(button);
         })
     })
     .catch((error) => {
