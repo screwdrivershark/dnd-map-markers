4f5146c00f49cf5320747aaa2c88857f5e6515ba
curious-broccoli <77789413+curious-broccoli@users.noreply.github.com>
2024-09-21 18:47:19 +0200

    hidden by default

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ fetch("markers.json")
                     <!-- <animate attributeName="stroke-width" values="1;0;1" dur="4s" repeatCount="indefinite"/> -->
                     <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6"/>                    
                 </svg>
-                <div class="popup">
+                <div class="popup hidden">
                     <h2>${marker.title}</h2>
                     <span>${description}</span>
                 </div>
